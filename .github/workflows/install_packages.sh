eb1b639cb4ee5fce4e365d5a11be3e22cce9c6e2
Xiao Yindong <xydarcher@uestc.edu.cn>
2023-10-22 21:45:22 +0800

    download packages for different arch.

---
+++ .github/workflows/install_packages.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e -x
+CWD=`pwd`
+MSYSTEM=$1
+ARCH=$2
+VERSION="current"
+TMP="${CWD}/tmp-${VERSION}"
+URL="https://repo.msys2.org/mingw/${MSYSTEM}"
+PKGS="
+mingw-w64-${ARCH}-gcc-13.1.0-7-any.pkg.tar.zst
+mingw-w64-${ARCH}-gcc-libs-13.1.0-7-any.pkg.tar.zst
+mingw-w64-${ARCH}-gcc-ada-13.1.0-7-any.pkg.tar.zst
+mingw-w64-${ARCH}-libgccjit-13.1.0-7-any.pkg.tar.zst
+mingw-w64-${ARCH}-gcc-fortran-13.1.0-7-any.pkg.tar.zst
+mingw-w64-${ARCH}-gcc-libgfortran-13.1.0-7-any.pkg.tar.zst
+mingw-w64-${ARCH}-gcc-objc-13.1.0-7-any.pkg.tar.zst
+mingw-w64-${ARCH}-ghdl-llvm-3.0.0.r147.g6c56631a7-2-any.pkg.tar.zst
+"
+
+if [ ! -d "${TMP}" ]; then
+    mkdir -p "${TMP}"
+fi
+cd ${TMP}
+for pkg in ${PKGS}; do
+    if [ ! -f "${pkg}" ]; then
+        wget "${URL}/${pkg}"
+    fi
+done
+pacman -U mingw-w64-${ARCH}-*-any.pkg.tar.*
